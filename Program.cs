ae4913d202e1b760afe200c72469ea4f3bcaf737
sisiphozono4 <sisiphozono4@gmail.com>
2026-09-06 12:10:07 +0200

    V3 - Add input validation with while loops

--- Program.cs
+++ Program.cs
@@ -7,33 +7,51 @@ Console.WriteLine("Please select a service below to get started.");
 Console.WriteLine("\nOUR SERVICES");
 Console.WriteLine("1. BOARDS\n2. CUPS\n3. STICKERS");
 Console.Write("\nSelect a service: ");
-int serviceOption = Convert.ToInt32(Console.ReadLine());
+string serviceInput =  Console.ReadLine();
+
+bool valid = int.TryParse(serviceInput, out int serviceOption);
+
+while (!valid || (serviceOption < 1 || serviceOption > 3))
+{
+   Console.Write("Please enter a numerical value e.g 1, 2, or 3: ");
+   serviceInput = Console.ReadLine();
+   valid = int.TryParse(serviceInput, out serviceOption);
+}
 
 string product = "";
 int price = 0;
 
 if (serviceOption == 1)
 {
-    Console.WriteLine("\nBOARDS");
-    Console.WriteLine("1. Welcome Board - R350\n2. Photo Booth - R450\n3. Canvas - R350");
-    Console.Write("\nSelect a board type: ");
-    int boardOption = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine("\nBOARDS");
+   Console.WriteLine("1. Welcome Board - R350\n2. Photo Booth - R450\n3. Canvas - R350");
+   Console.Write("\nSelect a board type: ");
+   string boardInput = Console.ReadLine();
+   
+   valid = int.TryParse(boardInput, out int boardOption);
 
-    if (boardOption == 1)
-    {
-       product = "Welcome Board";
-       price  = 350; 
-    } 
-    else if (boardOption == 2)
-    {
-       product = "Photo Booth";
-       price = 450;
-    }
-    else if (boardOption == 3)
-    {
-       product = "Canvas";
-       price = 350;
-    }
+   while (!valid || (boardOption < 1 || boardOption > 3))
+   {
+      Console.Write("Please select board 1, 2, or 3: ");
+      boardInput = Console.ReadLine();
+      valid = int.TryParse(boardInput, out boardOption);
+   }
+
+   if (boardOption == 1)
+   {
+      product = "Welcome Board";
+      price  = 350; 
+   } 
+   else if (boardOption == 2)
+   {
+      product = "Photo Booth";
+      price = 450;
+   }
+   else if (boardOption == 3)
+   {
+      product = "Canvas";
+      price = 350;
+   }
 
 }
 else if (serviceOption == 2)
@@ -41,63 +59,92 @@ else if (serviceOption == 2)
    Console.WriteLine("\nCUPS");
    Console.WriteLine("1. Personalized Cup - R10\n2. Cup with Lid - R15\n3. Personalized bottle - R75");
    Console.Write("\nSelect a cup type: ");
-   int cupOption = Convert.ToInt32(Console.ReadLine());
+   string cupInput = Console.ReadLine();
+
+   valid = int.TryParse(cupInput, out int cupOption);
+
+   while (!valid || (cupOption < 1 || cupOption > 3))
+   {
+      Console.Write("Please select cup 1, 2, or 3: ");
+      cupInput = Console.ReadLine();
+      valid = int.TryParse(cupInput, out cupOption);
+   }
+
    if (cupOption == 1)
-    {
-       product = "Personalized Cup";
-       price  = 10; 
-    }
+   {
+      product = "Personalized Cup";
+      price  = 10; 
+   }
    else if (cupOption == 2)
-    {
-       product = "Cup with Lid";
-       price = 15;
-    }
+   {
+      product = "Cup with Lid";
+      price = 15;
+   }
    else if (cupOption == 3)
-    {
-       product = "Personalized bottle";
-       price = 75;
-    }
+   {
+      product = "Personalized bottle";
+      price = 75;
+   }
 }
 else if (serviceOption == 3)
 {
-    Console.WriteLine("\nSTICKERS");
-    Console.WriteLine("1. Sticker Only - R6\n2. Hennessy bottle - R35\n3. jc le Roux bottle - R35\n4. Water bottle - R6\n5. Party pack bucket - R15\n6. Thank You - R4");
-    Console.Write("\nSelect a sticker type: ");
-    int stickerOption = Convert.ToInt32(Console.ReadLine());
-    if (stickerOption == 1)
-    {
-       product = "Sticker Only";
-       price  = 6; 
-    }
+   Console.WriteLine("\nSTICKERS");
+   Console.WriteLine("1. Sticker Only - R6\n2. Hennessy bottle - R35\n3. jc le Roux bottle - R35\n4. Water bottle - R6\n5. Party pack bucket - R15\n6. Thank You - R4");
+   Console.Write("\nSelect a sticker type: ");
+   string stickerInput = Console.ReadLine();
+
+   valid = int.TryParse(stickerInput, out int stickerOption);
+
+   while (!valid || (stickerOption < 1 || stickerOption > 6))
+   {
+      Console.Write("Please select sticker 1, 2, 3, 4, 5, or 6: ");
+      stickerInput = Console.ReadLine();
+      valid = int.TryParse(stickerInput, out stickerOption);
+   }
+
+   if (stickerOption == 1)
+   {
+      product = "Sticker Only";
+      price  = 6; 
+   }
    else if (stickerOption == 2)
-    {
-       product = "Hennessy bottle";
-       price = 35;
-    }
+   {
+      product = "Hennessy bottle";
+      price = 35;
+   }
    else if (stickerOption == 3)
-    {
-       product = "jc le Roux bottle";
-       price = 35;
-    }
+   {
+      product = "jc le Roux bottle";
+      price = 35;
+   }
     else if (stickerOption == 4)
-    {
-       product = "Water bottle";
-       price = 6;
-    }
-    else if (stickerOption == 5)
-    {
-        product = "Party pack bucket";
-        price = 15;
+   {
+      product = "Water bottle";
+      price = 6;
+   }
+   else if (stickerOption == 5)
+   {
+      product = "Party pack bucket";
+      price = 15;
     
-    }
-    else if (stickerOption == 6)
-    {
-        product = "Thank You";
-        price = 4;
-    }
+   }
+   else if (stickerOption == 6)
+   {
+      product = "Thank You";
+      price = 4;
+   }
 }
 Console.Write("Enter quantity: ");
-int quantity = Convert.ToInt32(Console.ReadLine());
+string quantityInput = Console.ReadLine();
+
+valid = int.TryParse(quantityInput, out int quantity);
+
+while (!valid || quantity <= 0)
+{
+   Console.Write("Enter a quantity greater than 0: ");
+   quantityInput = Console.ReadLine();
+   valid = int.TryParse(quantityInput, out quantity);
+}
 
 int total = price * quantity;
 
@@ -114,4 +161,3 @@ Console.WriteLine("1. Make a 50% deposit.\n2. Send your chosen image/design.\n3.
 Console.WriteLine("\nCONTACT DETAILS");
 Console.WriteLine("Contact: 063 xxx xxxx\nLocation: Gqeberha\nBank Name: Capitec\nAccount No: 1445xxxx");
 Console.WriteLine("\nNOTE: Collections can take up to 5 working days.\n\nThank you for your support!");
-
